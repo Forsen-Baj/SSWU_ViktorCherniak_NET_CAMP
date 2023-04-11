3fcdfb1398d5d53b277a3af0e8d42672ce9e12e3
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-11 09:30:33 +0300

    Update StringWorker.cs

--- Home_task_3/Home_task_3_2/StringWorker.cs
+++ Home_task_3/Home_task_3_2/StringWorker.cs
@@ -10,6 +10,7 @@ namespace Home_task_3_2
     public class StringWorker
     {
         //Find index of n-th substring in text
+       /// Молодець, що узагальнив!!!
         public int? IndexOfN(string text, string subString, int n)
         {
             if (n < 1)
@@ -42,7 +43,7 @@ namespace Home_task_3_2
         }
 
         public string ReplaceWordsWithDoubleLetters(string text, string replacement)
-        {
+        {// Можна спростити
             Regex regex = new Regex(@"\b\w*(\w)\1\w*\b");
             string newText = regex.Replace(text, replacement);
             return newText;
