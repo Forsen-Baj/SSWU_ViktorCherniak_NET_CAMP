8ec10f36afb98a23925763954aebf13c02bceb1e
Forsen-Baj <81097023+Forsen-Baj@users.noreply.github.com>
2023-04-15 00:52:31 +0300

    preps for 4.2

--- Home_task_4/Home_task_4_1/Program.cs
+++ Home_task_4/Home_task_4_1/Program.cs
@@ -7,29 +7,40 @@ string ukrainianText = @"Звичайно, радіо стало однією з
 програми: музику, новини, (спорт), розважальні шоу, ток-шоу тощо.
 Радіо можна 
 слухати в (автомобілі)
-, вдома або на вулиці з допомогою різних пристроїв: радіоприймачів, телефонів, планшетів, ноутбуків.
-За допомогою радіо можна отримати важл
+, вдома або на @вулиці з допомогою різних пристроїв: радіоприймачів, телефонів, планшетів, ноутбуків.
+За допомогою @радіо можна отримати важл
 иву інформацію про події у світі та в Україні.
 Також радіо може бути використане як засіб розваги та відпочинку, наприклад, під час довгих поїздок або на відпочинку на природі.
 Багато (радіостанцій пропонують ефір) зі спеціальними програмами для дітей, де можна послухати цікаві казки та ігри.
 Нині, в епоху інтернету, радіо продовжує бути популярним серед населення.
-Україна має багато власних (радіостанцій), які передають різноманітні програми та музику.
+Україна має багато власних (радіостанцій), які передають різноманітні @програми та музику.
 Радіо допомагає людям почуватися більш об'єднаними, слухаючи новини та програми з різних куточків країни.";
 
 string englishText = @"Lorem ipsum 
 dolor 
 sit amet, consectetur adipiscing (elit), sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+commodo consequat. Duis @aute irure dolor in reprehenderit in voluptate velit esse 
+cillum dolore eu fugiat nulla pariatur. @Excepteur sint occaecat cupidatat non
 
 
-proident, sunt in (culpa) qui officia deserunt mollit anim id est laborum.";
+proident, sunt in (culpa) qui officia deserunt mollit anim id est laborum.
+Random text to check email hello@world.com";
+
+string sampleText = "Hi @JohnDoe, how are you doing? Have you met @JaneDoe yet? hello@world. Please contact support@mycompany.com for further assistance. You can also follow us on Twitter at @MyCompany for updates.";
+
 
 var textProcessor = new TextProcessor();
 
 var sentences = textProcessor.FindAllSentencesWithBrackets(ukrainianText);
-Console.WriteLine(String.Join("\n\n", sentences));
+Console.WriteLine(String.Join("\n", sentences));
+
+Console.WriteLine( "\n\n");
 
 sentences = textProcessor.FindAllSentencesWithBrackets(englishText);
-Console.WriteLine(String.Join("\n\n", sentences));
+Console.WriteLine(String.Join("\n", sentences));
+
+Console.WriteLine("\n\n");
+
+sentences = textProcessor.FindAllWordsWithAt(sampleText);
+Console.WriteLine(String.Join("\n", sentences));
--- Home_task_4/Home_task_4_1/TextProcessor.cs
+++ Home_task_4/Home_task_4_1/TextProcessor.cs
@@ -9,28 +9,64 @@ namespace Home_task_4_1
 {
     public class TextProcessor
     {
-        public List<string> FindAllSentencesWithBrackets(string text)
+        private string[] ClearAndSplitText(string text, string[] separators)
         {
             string clearText = text;
-            clearText = clearText.Replace("\n", String.Empty);
-            clearText = clearText.Replace("\r", String.Empty);
-            clearText = clearText.Replace("\t", String.Empty);
+            clearText = clearText.Replace("\n", string.Empty);
+            clearText = clearText.Replace("\r", string.Empty);
+            clearText = clearText.Replace("\t", string.Empty);
 
+            //string[] separators = { ".", "?", "!" };
+            return clearText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+        public List<string> FindAllSentencesWithBrackets(string text)
+        {
             string[] separators = { ".", "?", "!" };
-            string[] sentences = clearText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
+            var sentences = ClearAndSplitText(text, separators);
             List<string> sentencesWithBrackets = new List<string>();
 
             foreach (string sentence in sentences)
-            {
                 if (sentence.Contains("(") && sentence.Contains(")"))
-                {
                     sentencesWithBrackets.Add(sentence.Trim());
-                }
-            }
 
             return sentencesWithBrackets;
+        }
 
+        public List<string> FindAllWordsWithAt(string text)
+        {
+            var words = text.Split(' ')
+                      .Where(word => word.Contains("@"));
+
+            var wordsWithAt = new List<string>();
+
+            foreach (var word in words)
+            {
+                wordsWithAt.Add(word);
+            }
+
+            return wordsWithAt;
+        }
+        public bool IsValidEmail(string email)
+        {
+            if (email.Length > 254)
+            {
+                Console.WriteLine(email + "RULE1");
+                return false;
+            }
+
+            if (email.Count(c => c == '@') != 1)
+            {
+                Console.WriteLine(email + "RULE2");
+                return false;
+            }
+
+            if (email.Any(c => c < 33))
+            {
+                Console.WriteLine(email + "RULE3");
+                return false;
+            }
+
+            return true;
         }
     }
 }
