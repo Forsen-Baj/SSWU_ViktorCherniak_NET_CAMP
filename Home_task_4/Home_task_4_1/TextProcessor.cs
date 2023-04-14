ca14f2b0638fae1c1d45cb1cfe37367e825d1bac
Forsen-Baj <81097023+Forsen-Baj@users.noreply.github.com>
2023-04-14 23:21:54 +0300

    task 4.1 done

---
+++ Home_task_4/Home_task_4_1/TextProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_4_1
+{
+    public class TextProcessor
+    {
+        public List<string> FindAllSentencesWithBrackets(string text)
+        {
+            string clearText = text;
+            clearText = clearText.Replace("\n", String.Empty);
+            clearText = clearText.Replace("\r", String.Empty);
+            clearText = clearText.Replace("\t", String.Empty);
+
+            string[] separators = { ".", "?", "!" };
+            string[] sentences = clearText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> sentencesWithBrackets = new List<string>();
+
+            foreach (string sentence in sentences)
+            {
+                if (sentence.Contains("(") && sentence.Contains(")"))
+                {
+                    sentencesWithBrackets.Add(sentence.Trim());
+                }
+            }
+
+            return sentencesWithBrackets;
+
+        }
+    }
+}
