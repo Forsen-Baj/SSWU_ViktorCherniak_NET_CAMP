022bd761d178854eae817f9db54b802d5ef61303
Forsen-Baj <81097023+Forsen-Baj@users.noreply.github.com>
2023-04-30 18:22:34 +0300

    6.2 done

---
+++ Home_task_6/Home_task_6_2/Sorter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Home_task_6_2
+{
+    public class Sorter
+    {
+        public IEnumerable<T> GetSorted<T>(params IEnumerable<T>[] lists)
+        {
+            List<T> result = new List<T>();
+
+            foreach (IEnumerable<T> list in lists)
+            {
+                result.AddRange(list);
+            }
+
+            result.Sort();
+
+            foreach (T item in result)
+            {
+                yield return item;
+            }
+        }
+    }
+}
