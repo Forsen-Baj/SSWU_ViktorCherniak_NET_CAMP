728cc3f1db9c99b7e2e5d25b06081ed2838e3423
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-02 10:20:53 +0300

    Update Sorter.cs

--- Home_task_6/Home_task_6_2/Sorter.cs
+++ Home_task_6/Home_task_6_2/Sorter.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace Home_task_6_2
 {
     public class Sorter
-    {
+    {// У цьому випадку краще цей метод зробити статичним. Але мені подобається його узагальнена сигнатура. Покажіть для всіх на занятті.
         public IEnumerable<T> GetSorted<T>(params IEnumerable<T>[] lists)
         {
             List<T> result = new List<T>();
