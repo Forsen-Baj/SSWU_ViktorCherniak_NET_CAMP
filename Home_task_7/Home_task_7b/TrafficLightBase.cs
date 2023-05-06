8e980f38164b62459fbba0d0119ba5172075e4f4
Forsen-Baj <81097023+Forsen-Baj@users.noreply.github.com>
2023-05-07 01:16:04 +0300

    task 7b done

---
+++ Home_task_7/Home_task_7b/TrafficLightBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_7b
+{
+    internal abstract class TrafficLightBase : ITrafficLight
+    {
+        public string Name { get; protected set; }
+        protected string[] colors;
+        protected int[] timeForEachState;
+        protected int currentState;
+        protected int timer = 0;
+
+        public TrafficLightBase(string name, string[] colors, int index, int[] timers)
+        {
+            Name = name;
+            this.colors = colors;
+            currentState = index;
+
+            if (timers.Length != colors.Length)
+                throw new ArgumentException();
+
+            timeForEachState = timers;
+        }
+
+        public void Tick(int time)
+        {
+            timer += time;
+            if (timer >= timeForEachState[currentState])
+            {
+                Switch();
+                timer = 0;
+            }
+        }
+        public abstract string GetCurrentColor();
+
+        private void Switch()
+        {
+            currentState = (currentState + 1) % colors.Length;
+        }
+    }
+}
---
+++ Home_task_7/Home_task_7b/TrafficLightController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_task_7b
+{
+    internal class TrafficLightController
+    {
+        private List<TrafficLightBase> trafficLights;
+
+        public TrafficLightController(IEnumerable<TrafficLightBase> trafficLights)
+        {
+            this.trafficLights = new List<TrafficLightBase>();
+
+            foreach (var trafficLight in trafficLights)
+                this.trafficLights.Add(trafficLight);
+        }
+
+        public void ImitateConsole(int interval, int maxTime = -1)
+        {
+            int currentTime = 0;
+
+            while (true)
+            {
+                if (currentTime == maxTime)
+                    break;
+
+                Console.WriteLine($"\nt={currentTime} с.");
+
+                foreach (var trafficLight in trafficLights)
+                {
+                    Console.WriteLine($"{trafficLight.Name}\t\t\t{trafficLight.GetCurrentColor()}");
+                    trafficLight.Tick(interval);
+                }
+
+                currentTime++;
+
+                Thread.Sleep(interval * 1000);
+            }
+        }
+    }
+}
