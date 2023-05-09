80e38f53461adc6a14574e23f28b338452c4aedf
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-09 12:48:35 +0300

    Update Program.cs

--- Home_task_7/Home_task_7b/Program.cs
+++ Home_task_7/Home_task_7b/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 Console.OutputEncoding = Encoding.UTF8;
 
 List<ClassicTrafficLight> trafficLights = new List<ClassicTrafficLight>() {
-    
+    // при довільній ініціалізації світлофори можуть бути неправильно синхронізовані.
     new ClassicTrafficLight("Північ-південь", 0, new int[] { 3, 2, 4 }),
     new ClassicTrafficLight("Південь-північ", 0, new int[] { 3, 2, 4 }),
     new ClassicTrafficLight("Схід-захід", 2, new int[] { 3, 2, 4 }),
