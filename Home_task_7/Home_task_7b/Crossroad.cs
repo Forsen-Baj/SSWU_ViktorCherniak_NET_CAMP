8511b6fb798e29f9c0988fd45827cfe817eea758
Forsen-Baj <81097023+Forsen-Baj@users.noreply.github.com>
2023-05-15 01:05:38 +0300

    Small visual adjustments

--- Home_task_7/Home_task_7b/Crossroad.cs
+++ Home_task_7/Home_task_7b/Crossroad.cs
@@ -6,28 +6,6 @@ using System.Threading.Tasks;
 
 namespace Home_task_7b
 {
-
-    internal abstract class IntersectionBase
-    {
-        public string Name { get; protected set; }
-        public List<TrafficLightBase> trafficLights;
-
-        public IntersectionBase(string name)
-        {
-            Name = name;
-            trafficLights = new List<TrafficLightBase>();
-        }
-
-        public void AddTrafficLight(TrafficLightBase trafficLight)
-        {
-            trafficLights.Add(trafficLight);
-        }
-
-        public override string ToString()
-        {
-            return Name;
-        }
-    }
     internal class Crossroad : IntersectionBase
     {
         
